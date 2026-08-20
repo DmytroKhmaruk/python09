3836d5b202fa9754646d3babf68a2c80cd74ab88
dkhmaruk <dkhmaruk@student.42vienna.com>
2026-08-20 17:16:11 +0200

    add discription at Field

--- ex0/space_station.py
+++ ex0/space_station.py
@@ -3,14 +3,17 @@ from datetime import datetime
 
 
 class SpaceStation(BaseModel):
-    station_id: str = Field(min_length=3, max_length=10)
-    name: str = Field(min_length=1, max_length=50)
-    crew_size: int = Field(ge=1, le=20)
-    power_level: float = Field(ge=0.0, le=100.0)
-    oxygen_level: float = Field(ge=0.0, le=100.0)
+    station_id: str = Field(min_length=3, max_length=10, description="3-10 "
+                            "characters")
+    name: str = Field(min_length=1, max_length=50, description="1-50 "
+                      "characters")
+    crew_size: int = Field(ge=1, le=20, description="1-20 people")
+    power_level: float = Field(ge=0.0, le=100.0, description="0-100 percent")
+    oxygen_level: float = Field(ge=0.0, le=100.0, description="0-100 percent")
     last_maintenance: datetime
     is_operational: bool = True
-    notes: str | None = Field(default=None, max_length=200)
+    notes: str | None = Field(default=None, max_length=200, description="max "
+                              "200 characters")
 
 
 def main() -> None:
